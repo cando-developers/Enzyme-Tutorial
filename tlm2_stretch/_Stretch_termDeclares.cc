5e7571b8a614a5712eb1800b9cfc277a14d2a965
meister <meister@thirdlaw.tech>
2023-06-19 16:44:45 -0400

    Added tlm2_stretch

---
+++ tlm2_stretch/_Stretch_termDeclares.cc
@@ -0,0 +1,111 @@
+/*
+    File: _Stretch_termDeclares.cc
+*/
+/*
+Open Source License
+Copyright (c) 2016, Christian E. Schafmeister
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ 
+This is an open source license for the CANDO software from Temple University, but it is not the only one. Contact Temple University at mailto:techtransfer@temple.edu if you would like a different license.
+*/
+/* -^- */
+#if USE_EXPLICIT_DECLARES
+// declare variables
+DECLARE_FLOAT(dhx1x1);
+DECLARE_FLOAT(dhx2x2);
+DECLARE_FLOAT(dhy1y1);
+DECLARE_FLOAT(dhy2y2);
+DECLARE_FLOAT(dhz1z1);
+DECLARE_FLOAT(dhz2z2);
+DECLARE_FLOAT(Energy);
+DECLARE_FLOAT(fx1);
+DECLARE_FLOAT(fx2);
+DECLARE_FLOAT(fy1);
+DECLARE_FLOAT(fy2);
+DECLARE_FLOAT(fz1);
+DECLARE_FLOAT(fz2);
+DECLARE_FLOAT(gx1);
+DECLARE_FLOAT(gx2);
+DECLARE_FLOAT(gy1);
+DECLARE_FLOAT(gy2);
+DECLARE_FLOAT(gz1);
+DECLARE_FLOAT(gz2);
+DECLARE_FLOAT(ohx1x2);
+DECLARE_FLOAT(ohx1y1);
+DECLARE_FLOAT(ohx1y2);
+DECLARE_FLOAT(ohx1z1);
+DECLARE_FLOAT(ohx1z2);
+DECLARE_FLOAT(ohx2y2);
+DECLARE_FLOAT(ohx2z2);
+DECLARE_FLOAT(ohy1x2);
+DECLARE_FLOAT(ohy1y2);
+DECLARE_FLOAT(ohy1z1);
+DECLARE_FLOAT(ohy1z2);
+DECLARE_FLOAT(ohy2z2);
+DECLARE_FLOAT(ohz1x2);
+DECLARE_FLOAT(ohz1y2);
+DECLARE_FLOAT(ohz1z2);
+DECLARE_FLOAT(StretchDeviation);
+DECLARE_FLOAT(tx44);
+DECLARE_FLOAT(tx45);
+DECLARE_FLOAT(tx46);
+DECLARE_FLOAT(tx47);
+DECLARE_FLOAT(tx48);
+DECLARE_FLOAT(tx49);
+DECLARE_FLOAT(tx50);
+DECLARE_FLOAT(tx51);
+DECLARE_FLOAT(tx52);
+DECLARE_FLOAT(tx53);
+DECLARE_FLOAT(tx54);
+DECLARE_FLOAT(tx55);
+DECLARE_FLOAT(tx56);
+DECLARE_FLOAT(tx57);
+DECLARE_FLOAT(tx58);
+DECLARE_FLOAT(tx59);
+DECLARE_FLOAT(tx60);
+DECLARE_FLOAT(tx61);
+DECLARE_FLOAT(tx62);
+DECLARE_FLOAT(tx63);
+DECLARE_FLOAT(tx64);
+DECLARE_FLOAT(tx65);
+DECLARE_FLOAT(tx66);
+DECLARE_FLOAT(tx67);
+DECLARE_FLOAT(tx68);
+DECLARE_FLOAT(tx69);
+DECLARE_FLOAT(tx70);
+DECLARE_FLOAT(tx71);
+DECLARE_FLOAT(tx72);
+DECLARE_FLOAT(tx73);
+DECLARE_FLOAT(tx74);
+DECLARE_FLOAT(tx75);
+DECLARE_FLOAT(tx76);
+DECLARE_FLOAT(tx77);
+DECLARE_FLOAT(tx78);
+DECLARE_FLOAT(tx79);
+DECLARE_FLOAT(tx80);
+DECLARE_FLOAT(tx81);
+DECLARE_FLOAT(tx82);
+DECLARE_FLOAT(tx83);
+DECLARE_FLOAT(tx84);
+DECLARE_FLOAT(tx85);
+DECLARE_FLOAT(tx86);
+DECLARE_FLOAT(tx87);
+DECLARE_FLOAT(tx88);
+DECLARE_FLOAT(tx89);
+
+
+#endif
--- tlm2_stretch/stretch.cc
+++ tlm2_stretch/stretch.cc
@@ -1,4 +1,7 @@
-#include "<stdio.h>"
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
 
 int enzyme_dup;
 int enzyme_out;
@@ -12,52 +15,110 @@ struct Stretch {
 };
 
 
-float stretch_energy(Stretch* stretch, float* pos, size_t num) {
-#define DECLARE_FLOAT(x) float x;
-#include "_StretchEnergy_declareCode.cc"
+
+extern double __enzyme_autodiff(void*, ...);
+
+void  dump_vec(const std::string& msg, float* deriv, size_t num) {
+  std::cout << msg << " ";
+  for ( int i=0; i<num; i++ ) {
+    std::cout << deriv[i] << " ";
+  }
+  std::cout << "\n";
+}
+
+#define power2(VAR) (VAR*VAR)
+#define mysqrt(VAR) (std::sqrt(VAR))
+#define reciprocal(VAR) (1.0/VAR)
+
+float stretch_energy(Stretch* stretch_begin, Stretch* stretch_end, float* pos) {
+#define USE_EXPLICIT_DECLARES 1
+#define DECLARE_FLOAT(VAR) float VAR;
+  float kb;
+  float r0;
+  float x1, y1, z1;
+  float x2, y2, z2;
+#include "_StretchEnergy_termDeclares.cc"
 #define STRETCH_SET_PARAMETER(VAR) { VAR = stretch->VAR; }
 #define STRETCH_SET_POSITION(VAR,IDX,OFFSET) { VAR = pos[IDX+OFFSET]; }
 #define STRETCH_ENERGY_ACCUMULATE(VAR) { result += VAR; }
   int I1;
   int I2;
-  float result;
+  float result = 0.0;
+  for ( auto stretch = stretch_begin; stretch < stretch_end; stretch++ ) {
 #include "_StretchEnergy_termCode.cc"
+  }
+#undef DECLARE_FLOAT
+#undef STRETCH_SET_PARAMETER
+#undef STRETCH_SET_POSITION
+#undef STRETCH_ENERGY_ACCUMULATE
   return result;
 }
 
-
-extern double __enzyme_autodiff(void*, ...);
-
-void  dump_vec(const std::string& msg, float* deriv, size_t num) {
-  std::cout << msg;
-  for ( int i=0; i<num; i++ ) {
-    std::cout << deriv[i] << " ";
-  }
-  std::cout "\n";
-}
-
 void grad_stretch_energy(Stretch* stretch_begin, Stretch* stretch_end, float* pos, float* deriv ) {
-  for ( auto stretch = stretch_begin; stretch < stretch_end; stretch++ ) {
-    __enzyme_autodiff( stretch_energy,
-                       enzyme_const, stretch,
-                       enzyme_dup, pos, deriv,
-                       enzyme_const num);
-  }
+  __enzyme_autodiff( (void*)stretch_energy,
+                     enzyme_const, stretch_begin,
+                     enzyme_const, stretch_end,
+                     enzyme_dup, pos, deriv );
 }
 
+float old_stretch_energy(Stretch* stretch_begin, Stretch* stretch_end, float* pos, float* deriv) {
+#define USE_EXPLICIT_DECLARES 1
+#define DECLARE_FLOAT(VAR) float VAR;
+  float kb;
+  float r0;
+  float x1, y1, z1;
+  float x2, y2, z2;
+#include "_Stretch_termDeclares.cc"
+#define STRETCH_SET_PARAMETER(VAR) { VAR = stretch->VAR; }
+#define STRETCH_SET_POSITION(VAR,IDX,OFFSET) { VAR = pos[IDX+OFFSET]; }
+#define STRETCH_ENERGY_ACCUMULATE(VAR) { result += VAR; }
+#define STRETCH_FORCE_ACCUMULATE(IDX,OFFSET,VAR) { deriv[IDX+OFFSET] += VAR; }
+  int I1;
+  int I2;
+  float result = 0.0;
+  bool calcForce = true;
+#define STRETCH_CALC_FORCE 1
+  for ( auto stretch = stretch_begin; stretch<stretch_end; stretch++ ) {
+#include "_Stretch_termCode.cc"
+  }
+#undef DECLARE_FLOAT
+#undef STRETCH_SET_PARAMETER
+#undef STRETCH_SET_POSITION
+#undef STRETCH_ENERGY_ACCUMULATE
+  return result;
+}
 
-int main( int argc, const char* argv[] ) {
-  float pos[12] = {0.0, 0.0, 0.0, 10.0, 0.0, 0.0,
-                   20.0, 0.0, 0.0, 25.0, 0.0, 0.0 };
-  float deriv[12];
-  Stretch stretch = { 10.0, 2.0, 0, 3, 20.0, 3.0, 6, 9 };
-
-  for (int i=0; i<12; i++ ) {
+void zeroVec(float* deriv, size_t num) {
+  for (int i=0; i<num; i++ ) {
     deriv[i] = 0.0;
   }
-  grad_stretch_energy( &stretch[0], &stretch[1], pos, deriv );
-
+}
+int main( int argc, const char* argv[] ) {
+  float pos[12] = {0.0, 19.0, 3.0, 10.0, 7.0, 80.0,
+                   20.0, 15.0, 17.0, 25.0, 44.0, 23.0 };
+  float deriv[12];
+  Stretch stretch[] = { {10.0, 2.0, 0, 3}, {20.0, 3.0, 6, 9} };
+  
   dump_vec("pos", pos, 12);
+  float energy = 0.0;
+  std::string arg1(argv[1]);
+  bool donew = (arg1 == "new");
+  size_t num = atoi(argv[2]);
+  if (donew) {
+    std::cout << "New method" << "\n";
+    for ( size_t nn = 0; nn<num; nn++ ) {
+      //      energy = stretch_energy( &stretch[0], &stretch[2], pos );
+      zeroVec(deriv,12);
+      grad_stretch_energy( &stretch[0], &stretch[2], pos, deriv );
+    }
+  } else {
+    std::cout << "Old method" << "\n";
+    for ( size_t nn = 0; nn<num; nn++ ) {
+      zeroVec(deriv,12);
+      energy = old_stretch_energy( &stretch[0], &stretch[2], pos, deriv );
+    }
+  }
+  std::cout << "Energy = " << energy << "\n";
   dump_vec("deriv", deriv, 12);
 
 }
